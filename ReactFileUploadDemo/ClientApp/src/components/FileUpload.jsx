19faf4d54def664c7b32cbb1197f0f7993cceb6b
Joel Shinness <jshinness@pnimedia.com>
2019-05-21 14:55:54 -0700

    It's A Demo, Y'All.

---
+++ ReactFileUploadDemo/ClientApp/src/components/FileUpload.jsx
@@ -0,0 +1,26 @@
+﻿import React, { useRef } from 'react';
+console.log(React.version);
+
+export function FileUpload() {
+  const fileInputRef = useRef();
+
+  function examineFiles() {
+    console.log(fileInputRef.current.files);
+
+    const data = new FormData(0);
+    data.append('filesISay', fileInputRef.current.files[0]);
+
+    fetch('/api/SampleData/file-upload-demo', {
+      method: 'POST',
+      body: data
+    }).then(response => {
+      console.log(response)
+    });
+  }
+  return <div>
+    <form>
+      <input accept='.csv' ref={fileInputRef} type='file' id='file-input'/>
+    </form>
+    <p><button onClick={examineFiles}>Files, you say?</button></p>
+  </div>
+}
